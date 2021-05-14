64b76eb1e7af9242b41d847b6f56721cb089d7ef
tuc289 <tuc289@psu.edu>
2021-05-14 09:36:19 -0400

    Compositional microbiome analysis R script

---
+++ Year2/CoDa.R
@@ -0,0 +1,4 @@
+# CoDa approach (Compositional analysis of Microbiome data) on year2 water samples - metagenomics (Taxonomic profile was processed using Kraken2 and Bracken)
+# Kraken github wiki (https://github.com/DerrickWood/kraken2/wiki)
+# Bracken github (https://github.com/jenniferlu717/Bracken)
+
